5209f9875b54d303ad1bf2d3535dc924a69630fa
Randall Sanborn <rhsanborn@gmail.com>
2025-09-10 13:55:58 -0400

    Switch tap and extra layer numbers so we can layer tap into extra. Try 2.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -8,6 +8,17 @@
 
 #define XXX &none
 
+#define U_BASE   0
+#define U_TAP    1
+#define U_EXTRA  2
+#define U_BUTTON 3
+#define U_NAV    4
+#define U_MOUSE  5
+#define U_MEDIA  6
+#define U_NUM    7
+#define U_SYM    8
+#define U_FUN    9
+
 // FPS friendly tap layer
 //#define miryoku_layer_game \
 //&kp tab,           &kp q,             &kp W,             &kp E,             &kp R,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
@@ -19,7 +30,7 @@
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 &kp A,             &kp R,             &kp S,             &kp T,             &kp G,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,             \
 &kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
-U_NP,              U_NP,              &kp ESC,        U_LT(U_EXTRA, SPACE), &kp TAB,           &kp RET,       &mt LC(LS(LA(LGUI))) BSPC,&u_to_U_BASE,   U_NP,              U_NP
+U_NP,              U_NP,              &kp ESC,           &lt U_EXTRA SPACE, &kp TAB,           &kp RET,       &mt LC(LS(LA(LGUI))) BSPC,&u_to_U_BASE,   U_NP,              U_NP
 
 #define MIRYOKU_LAYER_EXTRA \
 &kp F1,            &kp F2,            &kp F3,            &kp F4,            &kp GRAVE,         &kp LBKT,          &kp F7,            &kp F8,            &kp F9,            &kp RBKT,         \
@@ -28,13 +39,3 @@ U_NP,              U_NP,              &kp ESC,        U_LT(U_EXTRA, SPACE), &kp 
 U_NP,              U_NP,              &to U_BASE,        &kp SPC,           &none,             &kp MINUS,         &kp NUM_0,         &kp DOT,           U_NP,              U_NP
 
 
-#define U_BASE   0
-#define U_TAP    1
-#define U_EXTRA  2
-#define U_BUTTON 3
-#define U_NAV    4
-#define U_MOUSE  5
-#define U_MEDIA  6
-#define U_NUM    7
-#define U_SYM    8
-#define U_FUN    9
